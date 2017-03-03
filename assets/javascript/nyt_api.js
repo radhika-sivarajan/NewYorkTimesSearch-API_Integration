2f9d2174df8df6dcbb2e50157ac31b39fa961cb9
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-03-02 19:48:21 -0800

    Updated files

--- assets/javascript/nyt_api.js
+++ assets/javascript/nyt_api.js
@@ -7,7 +7,7 @@ var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
   authKey + "&q=";
 
-//
+//Function for API call and display retrived data
 function retriveNews(queryURL,count){
 
 	$.ajax({
@@ -15,35 +15,46 @@ function retriveNews(queryURL,count){
 		method: 'GET'
 	}).done(function(NTrecords){
 
-		//console.log(NTrecords,parseInt(count));
+		console.log(NTrecords,parseInt(count));
 
+		//If zero records
+		if(NTrecords.response.docs.length === 0){
+			$("#search-result").html("<div class='text-center' ><img src='assets/images/no-result.png' alt='No result'></img></div>");
+		}
+
+		//Display retrive aricle contents
 		for(var i = 0; i < parseInt(count); i++){
 
+			// New section for each record
 			var article = $("<div>");
 			article.attr("id","article-id-" + i);
 			$("#search-result").append(article);
 
 			if(NTrecords.response.docs[i].headline.main){
-			 	$("#article-id-" + i).append("<h4><span class='label label-primary'>" + (i+1) + ".</span> " + NTrecords.response.docs[i].headline.main +"</h4>");
+			 	$("#article-id-" + i).append("<h4 class='headline'><span class='label label-info'>" + (i+1) + ".</span> " + NTrecords.response.docs[i].headline.main +"</h4>");
 			}
-
 			if(NTrecords.response.docs[i].byline && NTrecords.response.docs[i].byline.original){
 				$("#article-id-" + i).append("<h6> " + NTrecords.response.docs[i].byline.original + "</h6>");
 			}
 
+			if(NTrecords.response.docs[i].pub_date){
+				$("#article-id-" + i).append("<span> Published date : " + NTrecords.response.docs[i].pub_date + "</span><br>");
+			}
+
 			if(NTrecords.response.docs[i].snippet){
-				$("#article-id-" + i).append("<span> " + NTrecords.response.docs[i].snippet + "</span>");
+				$("#article-id-" + i).append("Snippet : <span class='snippet'> " + NTrecords.response.docs[i].snippet + "</span><br>");
 			}
 
 			if(NTrecords.response.docs[i].web_url){
-				$("#article-id-" + i).append("<br><strong>Link : </strong><a href='" + NTrecords.response.docs[i].web_url + "' target='_blank'>Go to the page</a> OR ");
+				$("#article-id-" + i).append("<strong>Link : </strong><a href='" + NTrecords.response.docs[i].web_url + "' target='_blank'>Go to the page</a> OR ");
 
-				//Button for popup window to the Newyork times web page
-				var button = $("<button>").text("View").attr("id", "buton-" + i).addClass("view");
+				//Button for popup window to the Newyork times web page.
+				var button = $("<span>").text("View").attr("id", "buton-" + i).addClass("label label-success view");
 				button.attr("article-url",NTrecords.response.docs[i].web_url);
 				$("#article-id-" + i).append(button);
 			}
 
+			//Separator between retrieved article
 			$("#article-id-" + i).append("<hr>");
 			
 		}
@@ -61,13 +72,6 @@ function popupWindow(){
 //On submitting search parameters
 $("#submit").on("click", function(){
 
-	//Change layout, empty the result section and display.
-	$(".search-section ").removeClass("col-md-4 col-md-offset-4");
-	$(".search-section ").addClass("col-md-3 col-md-offset-1");
-	$(".result-section ").addClass("col-md-7");	
-	$("#search-result").empty();	
-	$(".result-section ").show();
-	
 	//Get the user inputs
 	searchTerm = $("#search-term").val().trim();
 	searchCount = $("#num-records").val().trim();
@@ -75,26 +79,37 @@ $("#submit").on("click", function(){
 	endYear = parseInt($("#end-year").val().trim());
 
 	//If empty search field alert
-	if(!searchTerm)
+	if(!searchTerm){
 		alert("Enter a search term");
+	}else{
 
-	//Attach search term to the query
-	queryURL = queryURLBase + searchTerm;
+		//Attach search term to the query
+		queryURL = queryURLBase + searchTerm;
 
-	//If the user has given start year or end year, attch them to the query
-	if(startYear)
-		queryURL += "&begin_date=" + startYear + "0101";
+		//If the user has given start year or end year, attach them to the query
+		if(startYear)
+			queryURL += "&begin_date=" + startYear + "0101";
+		if(endYear)
+			queryURL += "&end_date=" + endYear + "0101";
 
-	if(endYear)
-		queryURL += "&end_date=" + endYear + "0101";
+		//If the start year is greater than end year throw an alert
+		if(startYear && endYear && startYear>endYear){
+			alert("End year must be greater or equal to start year");
+		}
+		else{
 
-	//Pass final url along with the search count to the function
-	retriveNews(queryURL,searchCount);
+			//Change layout, empty the result section and display.
+			$(".search-section ").removeClass("col-md-4 col-md-offset-4");
+			$(".search-section ").addClass("col-md-4");
+			$(".result-section ").addClass("col-md-7 col-md-offset-1");	
+			$("#search-result").empty();
+			$(".result-section ").show();
 
-	// console.log(searchTerm,searchCount,startYear,endYear,queryURL);
-
+			//Pass final url along with the search count to the function
+			retriveNews(queryURL,searchCount);
+		}
+	}
 });
 
-//When click on view button on article section goto function for popup window
+//When click on view button on article, call function for popup window
 $(document).on("click",".view", popupWindow);
-
